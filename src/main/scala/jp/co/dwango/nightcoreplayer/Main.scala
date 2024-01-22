315bb423fd663a29520cfebf4cdc423122f9246f
Soichiro Yoshimura <yoshimura@soichiro.org>
2024-01-22 11:24:00 +0900

    14-1まで実装

--- src/main/scala/jp/co/dwango/nightcoreplayer/Main.scala
+++ src/main/scala/jp/co/dwango/nightcoreplayer/Main.scala
@@ -2,6 +2,7 @@ package jp.co.dwango.nightcoreplayer
 
 import java.io.File
 import javafx.application.Application
+import javafx.beans.value.{ChangeListener, ObservableValue}
 import javafx.geometry.Pos
 import javafx.scene.Scene
 import javafx.scene.control.Label
@@ -9,6 +10,7 @@ import javafx.scene.layout.{BorderPane, HBox}
 import javafx.scene.media.{Media, MediaPlayer, MediaView}
 import javafx.scene.paint.Color
 import javafx.stage.Stage
+import javafx.util.Duration
 
 object Main extends App:
   Application.launch(classOf[Main], args: _*)
@@ -19,14 +21,22 @@ class Main extends Application:
     val path = "/Users/soichiro_yoshimura/Movies/video.mp4"
     val media = Media(File(path).toURI.toString)
     val mediaPlayer = MediaPlayer(media)
+    val timeLabel = Label()
     mediaPlayer.setOnReady(new Runnable(){
-      override def run(): Unit = mediaPlayer.setRate(1.25)
+      override def run(): Unit =
+        mediaPlayer.setRate(1.25)
+        timeLabel.setText(formatTime(mediaPlayer.getCurrentTime, mediaPlayer.getTotalDuration))
     })
     mediaPlayer.play()
     val mediaView = MediaView(mediaPlayer)
     mediaView.setFitWidth(800)
     mediaView.setFitHeight(450)
-    val timeLabel = Label()
+
+    mediaPlayer.currentTimeProperty().addListener(new ChangeListener[Duration] {
+      override def changed(observable: ObservableValue[_ <: Duration], oldValue: Duration, newValue: Duration): Unit =
+        timeLabel.setText(formatTime(mediaPlayer.getCurrentTime, mediaPlayer.getTotalDuration))
+    })
+
     timeLabel.setText("00:00:00/00:00:00")
     timeLabel.setTextFill(Color.WHITE)
     val toolBar = HBox(timeLabel)
@@ -40,3 +50,12 @@ class Main extends Application:
     scene.setFill(Color.BLACK)
     primaryStage.setScene(scene)
     primaryStage.show()
+
+private[this] def formatTime(elapsed: Duration, duration: Duration): String =
+  "%02d:%02d:%02d/%02d:%02d:%02d".format(
+    elapsed.toHours.toInt,
+    elapsed.toMinutes.toInt % 60,
+    elapsed.toSeconds.toInt % 60,
+    duration.toHours.toInt,
+    duration.toMinutes.toInt % 60,
+    duration.toSeconds.toInt % 60)
